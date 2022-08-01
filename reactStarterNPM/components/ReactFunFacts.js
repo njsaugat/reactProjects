c943a8797bfc427bd4c7a5cf9a662df53ee31df7
Saugat Poudel <njsaugat@gmail.com>
2022-08-01 10:03:34 +0545

    react components added

---
+++ reactStarterNPM/components/ReactFunFacts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function ReactFunFacts() {
+  return (
+    <div>
+      <h2>Fun Facts about React</h2>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>created by Jordan Walke</li>
+        <li>100k stars on github</li>
+        <li>Maintained by Facebook</li>
+        <li>Powers thousands of apps</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ReactFunFacts;
