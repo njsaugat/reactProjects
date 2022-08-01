91bd15e818734a50634c4818543328feee74ffe4
Saugat Poudel <njsaugat@gmail.com>
2022-08-01 10:01:21 +0545

    index file added

--- reactStarterNPM/index.js
+++ reactStarterNPM/index.js
@@ -1,31 +1,16 @@
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<h1>Hello React</h1>);
-console.log(123);
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import Navbar from './components/Navbar';
+import ReactFunFacts from './components/ReactFunFacts';
+import ReasonsToLearnReact from './components/ReasonsToLearnReact';
 import App from './src/App';
 const center = <h1>Hello React</h1>;
-function Navbar() {
-  return (
-    <div className="navbar">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-        <li>Login</li>
-      </ul>
-    </div>
-  );
-}
-
-console.log(center);
-console.log(Navbar());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <div>
     <Navbar />
-    <App />
-    {center}
+    <ReasonsToLearnReact />
+    <ReactFunFacts />
   </div>
 );
