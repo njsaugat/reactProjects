c7fb094c95737729add2ae10206cd5db9b2111a0
Saugat Poudel <njsaugat@gmail.com>
2022-08-01 10:03:52 +0545

    reasons file added

---
+++ reactStarterNPM/components/ReasonsToLearnReact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function ReasonsToLearnReact() {
+  return (
+    <div className="reasons-react">
+      <h2>Reasons to learn React</h2>
+      <ul>
+        <li>Compasable</li>
+        <li>Declarative</li>
+        <li>Hireable Skill</li>
+        <li>Maintained by a lot</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ReasonsToLearnReact;
