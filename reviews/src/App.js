1a014d2102884ceffbcbae86fcb3ebfc8d75bc79
Saugat Poudel <njsaugat@gmail.com>
2022-08-07 13:33:15 +0545

    app js file added

--- reviews/src/App.js
+++ reviews/src/App.js
@@ -1,6 +1,16 @@
+import Review from './Review';
 import './style.css';
+import reviews from './UserReview';
 function App() {
-  return <div className="App">Project Starter</div>;
+  // const []
+
+  return (
+    <>
+      <h1>Our Reviews</h1>
+      <div className="highlight"></div>
+      <Review />;
+    </>
+  );
 }
 
 export default App;
