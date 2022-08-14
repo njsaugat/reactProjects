809403795d80bc92506f81aa41f4d23062339d97
Saugat Poudel <njsaugat@gmail.com>
2022-08-14 07:33:58 +0545

    app file added

---
+++ menu/src/App.js
@@ -0,0 +1,13 @@
+import MenuItems from './MenuItems';
+import './style.css';
+function App() {
+  return (
+    <>
+      <h1>Our Menu</h1>
+      <div className="highlight"></div>
+      <MenuItems />
+    </>
+  );
+}
+
+export default App;
