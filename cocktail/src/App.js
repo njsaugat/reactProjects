a65b4ac79ec6117bec0989449f76627fa262a6a7
Saugat Poudel <njsaugat@gmail.com>
2022-08-18 13:17:14 +0545

    app file added

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -1,6 +1,15 @@
+// import AboutUs from './AboutUs';
+import Cocktails from './Cocktails';
+import Navbar from './Navbar';
 import './style.css';
 function App() {
-  return <div className="App">Project Starter</div>;
+  return (
+    <div className="container">
+      <Navbar />
+      {/* <AboutUs /> */}
+      <Cocktails />
+    </div>
+  );
 }
 
 export default App;
