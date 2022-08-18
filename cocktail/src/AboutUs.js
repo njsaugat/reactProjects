a848dbf654f3d5433247e325af6cebf17384192c
Saugat Poudel <njsaugat@gmail.com>
2022-08-18 13:16:16 +0545

    about js file added

---
+++ cocktail/src/AboutUs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Us</h1>
+      <span className="about-us-content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut harum ea
+        dignissimos, assumenda corporis facere odit? Repudiandae optio natus
+        commodi et vel iusto debitis. Possimus similique eveniet sunt nihil
+        atque. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Mollitia, recusandae.
+      </span>
+    </div>
+  );
+}
