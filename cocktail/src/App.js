185c6b806a29e8b0479a9a28f50f263e7a2595c7
Saugat Poudel <njsaugat@gmail.com>
2022-08-20 14:19:58 +0545

    app file added

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -2,13 +2,30 @@
 import Cocktails from './Cocktails';
 import Navbar from './Navbar';
 import './style.css';
+import { Routes, Route } from 'react-router-dom';
+import AboutUs from './AboutUs';
 function App() {
   return (
-    <div className="container">
-      <Navbar />
-      {/* <AboutUs /> */}
-      <Cocktails />
-    </div>
+    <>
+      <div className="container">
+        <Navbar />
+        {/* <AboutUs /> */}
+        {/* {window.location.pathname === '/' && <Cocktails />}
+        {console.logwindow.location.pathname} */}
+        <Cocktails />
+      </div>
+      <Routes>
+        <Route
+          path="about-us"
+          element={
+            <>
+              {/* <Navbar /> */}
+              <AboutUs />
+            </>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
