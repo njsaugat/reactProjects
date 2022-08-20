4e04b3532ffeb2f3470bb8f8cb9b3189dae03d2a
Saugat Poudel <njsaugat@gmail.com>
2022-08-20 14:20:34 +0545

    navbar file added

--- cocktail/src/Navbar.js
+++ cocktail/src/Navbar.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import { Routes, Route } from 'react-router-dom';
+import AboutUs from './AboutUs';
+const GetHome = () => {
+  return <span className="home">Home</span>;
+};
+const GetAboutUs = () => {
+  return <span className="about">About</span>;
+};
+
+const GetLogo = () => {
+  return (
+    <div className="left branding">
+      The<span className="logo">Cocktail</span>DB
+    </div>
+  );
+};
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="left branding">
-        The<span className="logo">Cocktail</span>DB
-      </div>
-      <div className="right menus">
-        <span className="home">Home</span>
-        <span className="about">About</span>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <Link to="/">{<GetLogo />}</Link>
+        <div className="right menus">
+          <Link to="/">{<GetHome />}</Link>
+          <Link to="/about-us">{<GetAboutUs />}</Link>
+        </div>
+      </nav>
+      {/* <Routes>
+        <Route path="about-us" element={<AboutUs />} />
+      </Routes> */}
+    </>
   );
 }
