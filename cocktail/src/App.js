7d34d5bfe19eedcf68a5da698adba8df81bec9e0
Saugat Poudel <njsaugat@gmail.com>
2022-08-21 07:26:34 +0545

    app file added

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -1,28 +1,27 @@
-// import AboutUs from './AboutUs';
 import Cocktails from './Cocktails';
 import Navbar from './Navbar';
 import './style.css';
 import { Routes, Route } from 'react-router-dom';
 import AboutUs from './AboutUs';
+import Cocktail from './Cocktail';
+function Container({ Component }) {
+  return (
+    <div className="container">
+      <Navbar />
+      <Component />
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
-      <div className="container">
-        <Navbar />
-        {/* <AboutUs /> */}
-        {/* {window.location.pathname === '/' && <Cocktails />}
-        {console.logwindow.location.pathname} */}
-        <Cocktails />
-      </div>
       <Routes>
+        <Route path="/" element={<Container Component={Cocktails} />} />
+        <Route path="about-us" element={<Container Component={AboutUs} />} />
         <Route
-          path="about-us"
-          element={
-            <>
-              {/* <Navbar /> */}
-              <AboutUs />
-            </>
-          }
+          path="/cocktail/:id"
+          element={<Container Component={Cocktail} />}
         />
       </Routes>
     </>
