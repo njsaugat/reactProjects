abb8b209514dfcaf71b597afd3fb21b62cc49e0b
Saugat Poudel <njsaugat@gmail.com>
2022-08-22 07:27:09 +0545

    navbar file added

--- cocktail/src/Navbar.js
+++ cocktail/src/Navbar.js
@@ -19,18 +19,18 @@ const GetLogo = () => {
   );
 };
 
-const getSearchedItems = (e) => {
-  if (e.key === 'Enter') {
-    console.log(e.target.value);
-  }
-};
+// const getSearchedItems = (e) => {
+//   if (e.key === 'Enter') {
+//     console.log(e.target.value);
+//   }
+// };
 export default function Navbar() {
   return (
     <>
       <nav className="navbar">
         <Link to="/">{<GetLogo />}</Link>
         <div className="search">
-          <input type="text" onKeyDown={getSearchedItems} />
+          <input type="text" placeholder="Search a cocktail.." />
           <span>{searchIcon}</span>
         </div>
         <div className="right menus">
