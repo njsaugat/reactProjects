1b052815ae5328c983178bf9c484676699fe951f
Saugat Poudel <njsaugat@gmail.com>
2022-08-23 17:23:47 +0545

    app js file added

---
+++ randomPerson/src/App.js
@@ -0,0 +1,13 @@
+import RandomPerson from './RandomPerson';
+import './style.css';
+function App() {
+  return (
+    <>
+      <div className="top"></div>
+      <RandomPerson />
+      <div className="bottom"></div>
+    </>
+  );
+}
+
+export default App;
