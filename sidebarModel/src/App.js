bc0821e774282ab5e963469e1fd53d9f78ad4ebd
Saugat Poudel <njsaugat@gmail.com>
2022-08-27 11:17:42 +0545

    app file added

---
+++ sidebarModel/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+import ShowModel from './ShowModel';
+import './style.css';
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <ShowModel />
+    </div>
+  );
+}
+
+export default App;
