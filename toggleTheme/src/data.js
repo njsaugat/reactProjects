dc38be1a5cc37073b0ade4b12978dedb641d30da
Saugat Poudel <njsaugat@gmail.com>
2022-08-30 08:15:12 +0545

    data js file added

---
+++ toggleTheme/src/data.js
@@ -0,0 +1,29 @@
+const themeData = [
+  {
+    title: 'The WET Codbase',
+    date: 'Sunday 4th, 2020',
+    readTime: '11 min read',
+    newsData: 'Come waste your time with me',
+  },
+  {
+    title: 'Goodby, Clean Code',
+    date: 'Friday 22nd, 2019',
+    readTime: '5 min read',
+    newsData: 'Let clean code guide you. Then let it go.',
+  },
+  {
+    title: 'My Decade In Review',
+    date: 'Saturday 11th, 2018',
+    readTime: '5 min read',
+    newsData: 'A personal reflection.',
+  },
+
+  {
+    title: 'What Are The React Team Principles',
+    date: 'Thursday 4th, 2015',
+    readTime: '5 min read',
+    newsData: 'UI Before API.',
+  },
+];
+
+export default themeData;
