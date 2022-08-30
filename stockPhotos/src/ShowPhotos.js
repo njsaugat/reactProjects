696bf431d6b91d4b45a2375e2e1b947875b05798
Saugat Poudel <njsaugat@gmail.com>
2022-08-30 08:11:13 +0545

    show photo file added

--- stockPhotos/src/ShowPhotos.js
+++ stockPhotos/src/ShowPhotos.js
@@ -1,18 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import getPhotos from './GetPhoto';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+  // faCircleNotch,
+  faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ShowPhoto from './ShowPhoto';
 
+let page = 1;
+function getPageNumber() {
+  return page++;
+}
+const setPageNumberToOne = () => {
+  page = 1;
+};
 const searchIcon = <FontAwesomeIcon icon={faMagnifyingGlass} />;
+// const loadingIcon = <FontAwesomeIcon icon={faCircleNotch} />;
+
+function Loading() {
+  return <div className="loading">{/* <span>{loadingIcon}</span> */}</div>;
+}
+
+function ShowSearchResults({ searchTerm }) {
+  return (
+    <div className="search-results">
+      {`Search Results for`}
+      <span>"{searchTerm.toUpperCase()}" </span>
+    </div>
+  );
+}
 export default function ShowPhotos() {
   const [photos, setPhotos] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [scrollY, setScroll] = useState(window.scrollY);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setPageNumberToOne();
     (async () => {
-      const photosData = await getPhotos(searchTerm, 1);
+      const photosData = await getPhotos(searchTerm, getPageNumber());
       setPhotos(photosData);
     })();
+    return () => {
+      setPhotos([]);
+    };
   }, [searchTerm]);
 
   useEffect(() => {
@@ -24,19 +54,24 @@ export default function ShowPhotos() {
     window.addEventListener('scroll', changeScrollHeight);
 
     async function getExtraPhotos() {
-      const photosData = await getPhotos(searchTerm, 1);
+      const photosData = await getPhotos(searchTerm, getPageNumber());
       setPhotos((prevPhotos) => {
         return [...prevPhotos, ...photosData];
       });
     }
 
     if (scrollY + window.innerHeight > document.body.clientHeight) {
-      interval = setTimeout(getExtraPhotos, 2000);
+      interval = setTimeout(() => {
+        getExtraPhotos();
+        setLoading(false);
+      }, 2000);
+      setLoading(true);
     }
 
     return () => {
       window.removeEventListener('scroll', changeScrollHeight);
       clearInterval(interval);
+      // setSearchTerm('')
     };
   }, [scrollY, searchTerm]);
 
@@ -61,35 +96,13 @@ export default function ShowPhotos() {
         />
         <span className="search">{searchIcon}</span>
       </div>
+      {searchTerm && <ShowSearchResults searchTerm={searchTerm} />}
       <div className="photos">
-        {photos.map((photo) => {
-          return (
-            <div className="photo">
-              <img src={photo.urls.regular} alt="" srcSet="" />
-
-              <div className="details">
-                <div className="left">
-                  <span className="name">{photo.user.name}</span>
-                  <span className="likes">{photo.likes} </span>
-                </div>
-                <div className="right">
-                  <a
-                    href={photo.user.social.portfolio_url}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <img
-                      src={photo.user.profile_image.large}
-                      alt=""
-                      srcSet=""
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-          );
+        {photos.map((photo, index) => {
+          return <ShowPhoto photo={photo} key={index} />;
         })}
       </div>
+      {loading && <Loading />}
     </div>
   );
 }
