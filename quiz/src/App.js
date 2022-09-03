648724f58cf889771d29f4c7b377e7e28eeb3192
Saugat Poudel <njsaugat@gmail.com>
2022-09-03 20:08:44 +0545

    app file added

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,7 +1,33 @@
+import { useState } from 'react';
 import ShowForm from './ShowForm';
+import ShowQuestions from './ShowQuestions';
 import './style.css';
+
 function App() {
-  return <ShowForm />;
+  const [questions, setQuestions] = useState(0);
+  const [category, setCategory] = useState('');
+  const [difficulty, setDifficulty] = useState('');
+  function getDataFromForm({
+    totalQuestions,
+    selectedCategory,
+    selectedDifficulty,
+  }) {
+    setQuestions(totalQuestions.current.value);
+    setCategory(selectedCategory.current.value);
+    setDifficulty(selectedDifficulty.current.value);
+  }
+
+  return (
+    <>
+      <ShowForm getDataFromForm={getDataFromForm} />
+      {/* {console.log(questions, category, difficulty)} */}
+      <ShowQuestions
+        questions={questions}
+        category={category}
+        difficulty={difficulty}
+      />
+    </>
+  );
 }
 
 export default App;
