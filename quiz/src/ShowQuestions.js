56e51e6731780d879fc1bf9c690be8533d82685e
Saugat Poudel <njsaugat@gmail.com>
2022-09-04 18:44:38 +0545

    show question file added

--- quiz/src/ShowQuestions.js
+++ quiz/src/ShowQuestions.js
@@ -1,30 +1,9 @@
 import React, { useEffect, useState } from 'react';
-//politics-24
-//sports-21
-//history-23
+import { useRef } from 'react';
+import { getCategoryValue } from './getCategoryValue';
+import ShowModal from './ShowModal';
+import { shuffleArray } from './shuffleArray';
 
-const getCategoryValue = (category) => {
-  if (category.toLowerCase() === 'sports') {
-    return 21;
-  } else if (category.toLowerCase() === 'history') {
-    return 23;
-  } else if (category.toLowerCase() === 'politics') {
-    return 24;
-  }
-};
-
-function ShowModal() {
-  document.body.style.backgroundColor = 'black';
-  return (
-    <div className="finished-modal">
-      <h2>Congrats</h2>
-      You answered 50% of question correctly
-      <button className=" start next" onClick={() => window.location.reload()}>
-        Play Again
-      </button>
-    </div>
-  );
-}
 export default function ShowQuestions({ questions, category, difficulty }) {
   const API_URL = `https://opentdb.com/api.php?amount=${questions}&category=${getCategoryValue(
     category
@@ -33,10 +12,9 @@ export default function ShowQuestions({ questions, category, difficulty }) {
   const [quizQuestions, setQuizQuestions] = useState([]);
   const [questionIndex, setIndex] = useState(0);
   const [correctAnswer, setCorrectAnswer] = useState(0);
+  const questionRef = useRef(null);
+
   const getNextQuestion = () => {
-    if (questionIndex === questions - 1) {
-      return setIndex(9);
-    }
     setIndex(questionIndex + 1);
   };
   useEffect(() => {
@@ -47,29 +25,33 @@ export default function ShowQuestions({ questions, category, difficulty }) {
     }
     getQuizQuestions();
   }, [API_URL]);
-  //   console.log(questions, category, difficulty);
-  console.log(quizQuestions);
+
   if (quizQuestions === null || quizQuestions.length === 0) {
     return <div className="loader"></div>;
   }
-  // if (questionIndex === questions - 1) {
-  //   return <div className="finish">Finished</div>;
-  // }
   return (
     <div className="questions">
       {quizQuestions.map((quizQuestion, index) => {
         return (
           questionIndex === index && (
-            <div className="question form-control" key={index}>
+            <div
+              className="question form-control"
+              key={index}
+              ref={questionRef}
+            >
               <div className="state">
                 Correct Answer: {correctAnswer} / {questionIndex}{' '}
               </div>
-              <h1>{quizQuestion.question}</h1>
+              <h1>
+                {index + 1}. {quizQuestion.question}
+              </h1>
               <div className="answers">
-                {Array.from([
-                  ...quizQuestion.incorrect_answers,
-                  quizQuestion.correct_answer,
-                ]).map((answer) => {
+                {shuffleArray(
+                  Array.from([
+                    ...quizQuestion.incorrect_answers,
+                    quizQuestion.correct_answer,
+                  ])
+                ).map((answer) => {
                   return (
                     <button
                       className="answer start"
@@ -90,12 +72,17 @@ export default function ShowQuestions({ questions, category, difficulty }) {
               <button onClick={getNextQuestion} className="btn start next">
                 Next
               </button>
-              {console.log(questionIndex)}
             </div>
           )
         );
       })}
-      {questionIndex === questions - 1 && <ShowModal />}
+      {questionIndex === +questions && (
+        <ShowModal
+          questionRef={questionRef}
+          correctAnswer={correctAnswer}
+          questions={questions}
+        />
+      )}
     </div>
   );
 }
