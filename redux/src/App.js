eafef3c311f81355abfa0b6916e4309160bb78d4
Saugat Poudel <njsaugat@gmail.com>
2022-09-06 20:55:13 +0545

    app file added

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,6 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { decrementNumber, incrementNumber } from './actions';
 import './style.css';
+// import {useSelector,us}
+
 function App() {
-  return <div className="App">Project Starter</div>;
+  const numberState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <h1>Counter</h1>
+      <h2>With React & Redux</h2>
+      <button className="btn" onClick={() => dispatch(decrementNumber(5))}>
+        -
+      </button>
+      <span className="value">{numberState}</span>
+      <button className="btn" onClick={() => dispatch(incrementNumber(5))}>
+        +
+      </button>
+    </>
+  );
 }
 
 export default App;
