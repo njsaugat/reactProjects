9eb1d19cd3dd8ecddab0d64004f13293b9b16737
Saugat Poudel <njsaugat@gmail.com>
2022-09-07 10:20:43 +0545

    store file added

---
+++ redux/src/store.js
@@ -0,0 +1,11 @@
+import { legacy_createStore } from 'redux';
+
+// import {}
+// legacy_createStore
+// createSto
+
+import rootReducer from './reducers/rootReducer';
+
+const store = legacy_createStore(rootReducer);
+
+export default store;
