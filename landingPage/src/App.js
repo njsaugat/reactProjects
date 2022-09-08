8dac0ade6d27efe1661804561ac7987e7ae3517f
Saugat Poudel <njsaugat@gmail.com>
2022-09-08 10:07:39 +0545

    app js file added

---
+++ landingPage/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar';
+import Title from './components/Title';
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-black text-white h-screen">
+      <Navbar />
+      <Title />
+    </div>
+  );
+}
+
+export default App;
