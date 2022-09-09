feb3d4cd6f8bf806bf9741cd83dea6531e29cbea
Saugat Poudel <njsaugat@gmail.com>
2022-09-09 08:12:32 +0545

    app file added

--- landingPage/src/App.js
+++ landingPage/src/App.js
@@ -1,12 +1,26 @@
+import Business from './components/Business';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Payment from './components/Payment';
+import Promotion from './components/Promotion';
+import Review from './components/Review';
+import Stats from './components/Stats';
 import Title from './components/Title';
+import TryService from './components/TryService';
 import './index.css';
 
 function App() {
   return (
-    <div className="bg-black text-white h-screen">
+    <div className="bg-black text-white overflow-hidden  p-10 pr-0 md:pl-32">
       <Navbar />
       <Title />
+      <Stats />
+      <Business />
+      <Payment />
+      <Promotion />
+      <Review />
+      <TryService />
+      <Footer />
     </div>
   );
 }
