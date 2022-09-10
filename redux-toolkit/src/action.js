2f9fda1c464420a16e356b41c50df57f0197d6da
Saugat Poudel <njsaugat@gmail.com>
2022-09-10 20:39:39 +0545

    action file added

---
+++ redux-toolkit/src/action.js
@@ -0,0 +1,21 @@
+export const incrementCounter = () => {
+  return {
+    type: 'increment',
+  };
+};
+export const decrementCounter = () => {
+  return {
+    type: 'decrement',
+  };
+};
+
+export const multiplyCounter = () => {
+  return {
+    type: 'multiply',
+  };
+};
+export const divideCounter = () => {
+  return {
+    type: 'divide',
+  };
+};
