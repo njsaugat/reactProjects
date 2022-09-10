7dd41392ac61f514ebac539d8b18b6815977fcef
Saugat Poudel <njsaugat@gmail.com>
2022-09-10 20:39:06 +0545

    title file added

--- landingPage/src/components/Title.js
+++ landingPage/src/components/Title.js
@@ -31,32 +31,6 @@ const Title = () => {
       <div className="md:w-1/2      ">
         <img className=" object-cover" src={robotLayers} alt="" srcSet="" />
       </div>
-      {/* <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div class="md:flex">
-          <div class="md:shrink-0">
-            <img
-              class="h-48 w-full object-cover md:h-full md:w-48"
-              src={middleLayer}
-              alt="Man looking at item at a store"
-            />
-          </div>
-          <div class="p-8">
-            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-              Case study
-            </div>
-            <a
-              href="#"
-              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-            >
-              Finding customers for your new business
-            </a>
-            <p class="mt-2 text-slate-500">
-              Getting a new business off the ground is a lot of hard work. Here
-              are five ideas you can use to find your first customers.
-            </p>
-          </div>
-        </div>
-      </div> */}
     </main>
   );
 };
