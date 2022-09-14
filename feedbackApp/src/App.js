e0715c63e1821ee712ea9394ecaa91fde581536c
Saugat Poudel <njsaugat@gmail.com>
2022-09-14 15:00:43 +0545

    app file udpdated

--- feedbackApp/src/App.js
+++ feedbackApp/src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import FeedBack from './FeedBack';
 import Header from './Header';
 import './index.css';
+import Reviews from './Reviews';
 
 function App() {
+  const [reviews, setReviews] = useState([]);
   return (
-    <div className="flex flex-col justify-center items-center  bg-gradient-to-r from-indigo-600 to-blue-900 w-screen h-screen">
+    <div className="flex relative flex-col justify-center items-center  bg-gradient-to-r from-indigo-600 to-blue-900 w-screen min-h-screen overflow-x-hidden">
       <Header />
-      <FeedBack />
+      <FeedBack setReviews={setReviews} />
+      {reviews && <Reviews reviews={reviews} />}
     </div>
   );
 }
