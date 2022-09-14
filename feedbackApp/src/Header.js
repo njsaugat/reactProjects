070bf1bbd4f5b11cdbd1ca90de2373a4d49248e4
Saugat Poudel <njsaugat@gmail.com>
2022-09-14 15:01:26 +0545

    header file added

--- feedbackApp/src/Header.js
+++ feedbackApp/src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="flex fixed top-0 bg-gradient-to-l from-indigo-700 to-blue-800 text-orange-500 text-xl font-bold justify-center py-3 w-screen  self-start ">
+    <div className="flex absolute top-0 bg-gradient-to-l from-indigo-700 to-blue-800 text-orange-500 text-xl font-bold justify-center py-3 w-screen  self-start ">
       Feedback
     </div>
   );
