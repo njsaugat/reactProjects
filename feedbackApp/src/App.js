8f48fc913c11034c5e033fa4575132a4fa5dc7a0
Saugat Poudel <njsaugat@gmail.com>
2022-09-16 11:21:58 +0545

    app file added

--- feedbackApp/src/App.js
+++ feedbackApp/src/App.js
@@ -1,31 +1,14 @@
-import { useState } from 'react';
-import FeedBack from './FeedBack';
-import Header from './Header';
 import './index.css';
-import MetaInfo from './MetaInfo';
-import Reviews from './Reviews';
+import { Route, Routes } from 'react-router-dom';
+import Container from './Container';
+import About from './About';
 
 function App() {
-  const [reviews, setReviews] = useState([]);
-  const [editIndex, setEditIndex] = useState(-1);
   return (
-    <div className="flex relative flex-col justify-center items-center  bg-gradient-to-r from-indigo-600 to-blue-900 w-screen min-h-screen overflow-x-hidden">
-      <Header />
-      <FeedBack
-        reviews={reviews}
-        setReviews={setReviews}
-        editIndex={editIndex}
-        setEditIndex={setEditIndex}
-      />
-      {reviews && <MetaInfo reviews={reviews} />}
-      {reviews && (
-        <Reviews
-          reviews={reviews}
-          setReviews={setReviews}
-          setEditIndex={setEditIndex}
-        />
-      )}
-    </div>
+    <Routes>
+      <Route path="/" element={<Container />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
