434f6ee7d8d4fb901d4663f8ad28d68f3a15e97a
Saugat Poudel <njsaugat@gmail.com>
2022-09-16 11:21:44 +0545

    about file added

---
+++ feedbackApp/src/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+const About = () => {
+  return (
+    <div className="flex relative flex-col justify-center items-center  bg-gradient-to-r from-indigo-600 to-blue-900 w-screen min-h-screen overflow-x-hidden">
+      <Header />
+      About
+    </div>
+  );
+};
+
+export default About;
