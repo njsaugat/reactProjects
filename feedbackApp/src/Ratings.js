d4b90c7a599b55b74c44a5ec6d4c9d3fcf719ccb
Saugat Poudel <njsaugat@gmail.com>
2022-09-15 21:30:20 -0600

    ratings file added

---
+++ feedbackApp/src/Ratings.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState } from 'react';
+import buttonProperties from './buttonProperties';
+
+const ratingsArr = Array.from(Array(10).keys());
+
+const selected = ' bg-slate-800 text-white';
+
+const Ratings = ({ selectedReview, setSelectedReview }) => {
+  const [buttonStyle, setButtonStyle] = useState(buttonProperties);
+
+  return (
+    <>
+      {ratingsArr.map((rating, index) => {
+        return (
+          <div
+            className={
+              selectedReview === index + 1 ? buttonStyle : buttonProperties
+            }
+            key={index}
+            onClick={(e) => {
+              setButtonStyle((prevProperties) => prevProperties + selected);
+              setSelectedReview(+e.target.innerText);
+            }}
+          >
+            {rating + 1}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Ratings;
