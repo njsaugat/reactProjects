e360e4cefb225756af7027420c46aee6df0d772a
Saugat Poudel <njsaugat@gmail.com>
2022-09-16 11:22:11 +0545

    button properties updated

--- feedbackApp/src/buttonProperties.js
+++ feedbackApp/src/buttonProperties.js
@@ -1,3 +1,3 @@
-const buttonProperties = `rating cursor-pointer w-8 h-8 flex justify-center items-center m-1.5 rounded-full    bg-slate-300`;
+const buttonProperties = `rating cursor-pointer w-8 h-8 flex justify-center items-center m-1.5 rounded-full hover:bg-gradient-to-b  from-orange-400 to-pink-400 hover:transition-all    bg-slate-300  `;
 
 export default buttonProperties;
