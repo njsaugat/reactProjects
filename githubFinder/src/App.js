8a00aa65408b6aa26b22e8a6c5d930b8e1ba4565
Saugat Poudel <njsaugat@gmail.com>
2022-09-17 15:47:03 +0545

    app file added

---
+++ githubFinder/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import Container from './searchUsers/Container';
+import Navbar from './Navbar';
+import './style.css';
+import Users from './searchUsers/Users';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ContainerUserProfile from './userProfile/Container';
+
+const GithubSearcher = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [users, setUsers] = useState([]);
+  return (
+    <div className=" bg-gradient-to-b from-gray-600 to-slate-700 w-screen min-h-screen">
+      <Navbar />
+      <Container setSearchTerm={setSearchTerm} />
+      {users && (
+        <Users users={users} setUsers={setUsers} searchTerm={searchTerm} />
+      )}
+    </div>
+  );
+};
+
+function App() {
+  // const [searchTerm, setSearchTerm] = useState('');
+  //   const [users, setUsers] = useState([]);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<GithubSearcher />} />
+        <Route path="/user" element={<ContainerUserProfile />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
